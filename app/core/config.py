eb86bbd18131f2ed229bf25a9c52560eb8262410
Saburov Gleb <saburovgleb@gmail.com>
2022-09-02 21:07:34 +0300

    Мелкая правка конфига и зависимостей

--- app/core/config.py
+++ app/core/config.py
@@ -11,13 +11,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 class Config():
    DEBUG = False
 
-   SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-   SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-
-class DevelopmentConfig(Config):
-   DEBUG = True
    DB_HOST = os.environ.get('DATABASE_HOST', 'localhost')
    DB_PORT = os.environ.get('DATABASE_PORT', 5432)
    DB_USER = os.environ.get('DATABASE_USERNAME')
@@ -25,6 +18,13 @@ class DevelopmentConfig(Config):
    DB_NAME = os.environ.get('DATABASE_NAME')
 
    SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+   SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+
+class DevelopmentConfig(Config):
+   DEBUG = True
+   # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 
 class ProductionConfig(Config):
